27f09237b5ae3ef7fb40758756321cfa0b76de69
Aletz <amanriquez@next-cloud.mx>
2018-02-07 21:08:02 -0600

    estilos header y como funciona

---
+++ views/scripts/script-pakke.js
@@ -0,0 +1,39 @@
+console.log('::::: ::: pakke 7 feb 18');
+
+
+
+
+function menu() {
+
+	var opennn = false;
+
+	$('.menu-btn a').on('click', function(e){
+		e.preventDefault();
+		if(opennn === false) {
+
+			$('#menu').addClass('activo');
+			$('#menu').fadeIn('swing');
+			
+			opennn = true;
+		} else {
+
+			$('#menu').fadeOut('fast');
+		
+
+			opennn = false;
+		}
+	})
+	$('#menu li a').on('click', function(){
+		$('#menu').fadeOut('fast');
+		$('#menu').removeClass('activo');
+		opennn = false;
+	})
+
+
+}
+
+
+
+
+
+menu();
