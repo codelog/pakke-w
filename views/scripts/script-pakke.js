24e2e0b02edf669fd10138097e83735b5838b67c
Aletz <amanriquez@next-cloud.mx>
2018-02-09 22:55:16 -0600

    menu sticky

--- views/scripts/script-pakke.js
+++ views/scripts/script-pakke.js
@@ -2,10 +2,12 @@ console.log('::::: ::: pakke 7 feb 18');
 
 
 
-// MENU
+// MENU RESPONSIVE
 function menu() {
 
+	var eventFired = 0;
 	var opennn = false;
+
 
 	$('.menu-btn a').on('click', function(e){
 		e.preventDefault();
@@ -13,25 +15,77 @@ function menu() {
 
 			$('#menu').addClass('activo');
 			$('#menu').fadeIn('swing');
-			
+
 			opennn = true;
+
 		} else {
 
 			$('#menu').fadeOut('fast');
-		
 
 			opennn = false;
 		}
 	})
 	$('#menu li a').on('click', function(){
+
 		$('#menu').fadeOut('fast');
 		$('#menu').removeClass('activo');
+
 		opennn = false;
 	})
+
+
+
+
+	$(window).on('resize', function() {
+		if (!eventFired) {
+			if ($(window).width() === 1000) {
+
+				$('#menu').fadeOut('fast');
+				opennn = false;
+
+			} else if ($(window).width() > 1000) {
+
+				$('#menu').fadeIn('fast');
+				opennn = false;
+
+			} 
+		}
+	});
+
 }
 
 
-// MENU BAJA
+// PARA MENU FIJO STICKY
+function detectarScroll() {
+	$(window).on('scroll', function(){
+		var paraMenu = $(window).scrollTop();
+		if(paraMenu > 230){
+			$('#menu-scroll').addClass('activo');
+		} else{
+			$('#menu-scroll').removeClass('activo');
+		};
+	});
+};
+
+
+// MENU EFECTO SCROLL ID
+function menuscroll() {
+	jQuery('#menu li a[href^="#"]').on('click', function() {
+		
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = jQuery(this.hash);
+			target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				jQuery('html, body').animate({
+					scrollTop: target.offset().top - 70
+				}, 1000);
+				return false;
+			}
+		}
+	});
+}
+
+
 
 
 // SLIDER TESTIMONIOS
@@ -42,11 +96,21 @@ function menu() {
 
 // FORM NEWSLETTER
 
+function widthmenu() {
+
+	
+}
 
 
 
 
+setTimeout(function(){
+	menuscroll();
+	detectarScroll();
+	menu();
+	widthmenu();
+}, 250);
 
 
 
-menu();
+
