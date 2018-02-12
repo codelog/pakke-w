8dccc325e3e916277b44ef5b0a15fa99a0556547
Aletz <amanriquez@next-cloud.mx>
2018-02-12 14:15:13 -0600

    ventana video, mapa por afuera

--- views/scripts/script-pakke.js
+++ views/scripts/script-pakke.js
@@ -1,7 +1,6 @@
 console.log('::::: ::: pakke 7 feb 18');
 
 
-
 // MENU RESPONSIVE
 function menu() {
 
@@ -86,6 +85,48 @@ function menuscroll() {
 }
 
 
+// VIDEO
+function video() {
+	
+	$('#videoBtn').on('click', function(e){
+		e.preventDefault();
+		$('#video .video').append('<iframe src="https://www.youtube.com/embed/zjFevJen1dY?rel=0&amp;showinfo=0?ecver=1" allowfullscreen></iframe>');
+		$('body').addClass('videoWp');
+		$('#video').fadeIn('swing');
+	});
+	$('.btn-close, #video .bg').on('click', function(e){
+		e.preventDefault();
+		
+		$('#video').fadeOut('swing');
+		$('body').removeClass('videoWp');
+
+		setTimeout(function(){
+			$('#video .video iframe').remove();
+		}, 800);	
+	})
+	$(document).keyup(function(e) {
+		if (e.keyCode === 27) {
+			
+			$('#video').fadeOut('swing');
+			$('body').removeClass('videoWp');
+
+			setTimeout(function(){
+				$('#video .video iframe').remove();
+			}, 800);
+		}
+	});
+}
+
+
+// MAPA CONTACTO
+function mapaContacto() {
+
+	$('#contacto .mapa').append('<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15053.490181359532!2d-99.22707!3d19.396303!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d201a44b349ffb%3A0xc2a6de81ea87b73f!2sAv+Constituyentes+908%2C+Lomas+Altas%2C+11950+Ciudad+de+M%C3%A9xico%2C+CDMX%2C+M%C3%A9xico!5e0!3m2!1ses!2sus!4v1486745337249" allowfullscreen></iframe>');
+}
+
+
+
+
 
 
 // SLIDER TESTIMONIOS
@@ -96,10 +137,7 @@ function menuscroll() {
 
 // FORM NEWSLETTER
 
-function widthmenu() {
 
-	
-}
 
 
 
@@ -109,7 +147,12 @@ setTimeout(function(){
 	detectarScroll();
 	menu();
 	widthmenu();
+	video();
 }, 250);
+
+setTimeout(function(){
+	mapaContacto();
+}, 1000);
 
 
 
